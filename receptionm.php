b2737fb7820b7669abfee40689ecdf78455071a1
elbahaouiimane <imane32321@gmail.com>
2024-07-16 17:05:22 +0100

    ll

---
+++ receptionm.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reception de marchandises</title>
+    <link rel="stylesheet" href="accstyle.css">
+</head>
+<body>
+<header>
+        <div class="nav-container">
+            <img src="image/logol.png" alt="Logo" class="logo">
+            <nav>
+                <ul>
+                    <li><a href="#">Reception de Marchandises</a></li>
+                    <li><a href="#">Deconnexion</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="divider"></div>
+    </header>
+    
+    <h1>nom Fournisseur + date d'achat(element doient etre aligner)</h1> 
+    
+    
+    <table>
+    <tr>
+        <th>Numéro d'article</th>
+        <th>Description d'article</th>
+        <th>Quantité</th>
+        <th>Prix unitaire</th>
+        <th>Magasin</th>
+        <th>Code UM</th>
+        <th>Total brut (DI)</th>
+        <th>Actions</th>
+    </tr>
+    <tr>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>è</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>-</td>
+        <td></td>
+        <td></td>
+        <td></td>
+
+    </tr>
+    <tr>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>è</td>
+        <td>--</td>
+        <td></td>
+        <td></td>
+        <td></td>
+
+    </tr>
+
+
+
+    </table>
+    
+</body>
+</html>
