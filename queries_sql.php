823c6dbb7cbfc8e1dc5d904cc9ac5effccf849da
elbahaouiimane <imane32321@gmail.com>
2024-07-22 16:16:47 +0100

    sl

---
+++ queries_sql.php
@@ -0,0 +1,13 @@
+
+<?php
+$serverName = "serverName\\sqlexpress"; //serverName\instanceName
+$connectionInfo = array( "Database"=>"dbName", "UID"=>"sa", "PWD"=>"bpsmaroc");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if( $conn ) {
+     echo "Connection established.<br />";
+}else{
+     echo "Connection could not be established.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+?>
